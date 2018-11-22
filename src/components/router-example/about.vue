1bc3db2b624955fae1017f6916f152a4cea79f9a
Zhang Yuya <primeage@126.com>
2018-11-22 17:48:22 +0800

    Event: 模板语法 2018.11.22 Name：Yuya Zhang DateTime: 2018.11.15 Address: Company

--- src/components/router-example/about.vue
+++ src/components/router-example/about.vue
@@ -3,6 +3,18 @@
     <h1>about</h1>
     <p>{{ msg }}</p>
     <input type="text">
+
+    <!--计算属性和侦听器-->
+    <h4-title>计算属性和侦听器</h4-title>
+    <div id="example">
+      {{ message.split('').reverse().join('') }}
+    </div>
+    <hr>
+    <h4-title>计算属性和侦听器-实例2</h4-title>
+    <div id="example1">
+      <p>Original message: " {{ message }}"</p>
+      <p>Computed reversed message: "{{ reversedMessage }}</p>
+    </div>
   </div>
 </template>
 
@@ -11,8 +23,20 @@ export default {
   name: 'about',
   data () {
     return {
-      msg: '我是 about 组件'
+      msg: '我是 about 组件',
+      message: 'application is running!',
+      firstName: 'Foo',
+      lastName: 'Bar',
+      fullName: 'Foo Bar'
     }
+  },
+  computed: {
+    reversedMessage: function () {
+      return this.message.split(' ').reverse().join(' ')
+    }
+  },
+  watch: {
+
   }
 }
 </script>
